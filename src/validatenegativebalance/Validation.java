f949bf33e28a1e66dd70e60c09c0474784ada1c9
Breno Borges <breno.borges.15@outlook.com>
2025-01-29 15:05:33 -0300

    Implementação de mais uma condição na validação dos saldos negativos

--- src/validatenegativebalance/Validation.java
+++ src/validatenegativebalance/Validation.java
@@ -11,29 +11,30 @@ import java.util.List;
  */
 public class Validation {
     
+    
     public static ResultField validateNegativeColumnAcumulo(String filePath) throws IOException {
         // Traz os valores das colunas que preciso para validação
         List<Double> listAcumulate = GetColumn.getColumnAcumuloValues(filePath);
         List<String> operations = GetColumn.getColumnOperacaoValues(filePath);
         List<Double> transactions = GetColumn.getColumnTransacaoValues(filePath);
         List<Double> points = GetColumn.getColumnPointsValues(filePath);
-        int indexNegativeValueFounded = findNegativeBalance(filePath);
+        int indexNegativeValueFound = findNegativeBalance(filePath);
 
-        DecimalFormat decimalFormat = new DecimalFormat("#"); // Converte a notação cientifica
+        DecimalFormat decimalFormat = new DecimalFormat("#");
 
         // Pensar em uma forma de exibir todas as transações
-        return new ResultField(listAcumulate.get(indexNegativeValueFounded),
-                operations.get(indexNegativeValueFounded),
-                decimalFormat.format(transactions.get(indexNegativeValueFounded)),
-                points.get(indexNegativeValueFounded),
-                indexNegativeValueFounded);
+        return new ResultField(listAcumulate.get(indexNegativeValueFound),
+                operations.get(indexNegativeValueFound),
+                decimalFormat.format(transactions.get(indexNegativeValueFound)),
+                points.get(indexNegativeValueFound),
+                indexNegativeValueFound);
     }
 
     private static int findNegativeBalance(String filePath) throws IOException {
         List<Double> listAcumulate = GetColumn.getColumnAcumuloValues(filePath);
         List<String> operations = GetColumn.getColumnOperacaoValues(filePath);
         List<Double> points = GetColumn.getColumnPointsValues(filePath);
-        int indexNegativeValueFounded = -1;
+        int indexNegativeValueFound = -1;
         int index = listAcumulate.size() - 1;
         boolean foundNegativeWithoutRefund = false;
 
@@ -42,33 +43,39 @@ public class Validation {
             // Encontrar valor negativo na coluna acumulo desde que a operação não sejam estorno de credito
             for (int i = index; i >= 0; i--) { // Começa da última linha e ignora o cabeçalho
                 if (listAcumulate.get(i) < 0 && !operations.get(i).equals("ESTORNO DE CRÉDITO")) {
-                    indexNegativeValueFounded = i;
+                    indexNegativeValueFound = i;
                     break;
                 }
             }
 
             //Verifica a sequencia da lista após o primeiro valor negativo
             boolean hasRefund = false;
-            for (int i = indexNegativeValueFounded - 1; i >= 0; i--) { // Começa ddo próximo índice após encontrar o negativo
+            for (int i = indexNegativeValueFound - 1; i >= 0; i--) { // Começa ddo próximo índice após encontrar o negativo
                 // Verifica se houve algum estorno do valor negativo encontrado
-                if (points.get(i) == (points.get(indexNegativeValueFounded) * -1)
+                if (points.get(i) == (points.get(indexNegativeValueFound) * -1)
                         && operations.get(i).equals("ESTORNO DE CRÉDITO")
                         || operations.get(i).equals("ESTORNO DE EXPIRAÇÃO")) {
                     hasRefund = true;
                     index = i - 1;
+                    break;
+                } else if(operations.get(indexNegativeValueFound).equals("TRANSFERÊNCIA")
+                        && points.get(indexNegativeValueFound) == (points.get(indexNegativeValueFound + 2) * -1)){
+                    hasRefund = true;
+                    index = i - 1;
+                    break;
                 }
             }
 
             // Verifica se não houve estorno do valor negativo encontrado
             if (!hasRefund) {
-                for (int i = indexNegativeValueFounded - 1; i >= 0; i--) {
+                for (int i = indexNegativeValueFound - 1; i >= 0; i--) {
                     foundNegativeWithoutRefund = false;
                     // Simula a transformação da coluna PONTOS em zero
-                    List<Double> newBalance = reloadBalance(listAcumulate, points, indexNegativeValueFounded);
-
+                    List<Double> newBalance = reloadBalance(listAcumulate, points, indexNegativeValueFound);
+                    
                     // Verifica se o saldo ficará positivo após a simulação
                     if (newBalance.getFirst() >= 0) {
-                        return indexNegativeValueFounded; // Retorna o índice da linha onde a pessoa ficou com saldo negativo
+                        return indexNegativeValueFound; // Retorna o índice da linha onde a pessoa ficou com saldo negativo
                     }
                 }
             }
